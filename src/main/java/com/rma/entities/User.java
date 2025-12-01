d4ac26973bbc30e3b8efb9fac012ac6ca520c4f7
RAFAEL MARANDUBA DE ALMEIDA <127874021+rmalmeidax@users.noreply.github.com>
2025-12-01 06:41:18 -0300

    Instalação Banco H2

--- src/main/java/com/rma/entities/User.java
+++ src/main/java/com/rma/entities/User.java
@@ -3,21 +3,33 @@ package com.rma.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tab_user")
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+	
+
+
+
 	private String email;
 	private String phone;
 	private String password;
 	
 	
-	public User() {
-		
-	}
+	public User() {}
 
 
 	public User(Long id, String name, String email, String phone, String password) {
@@ -50,12 +62,12 @@ public class User implements Serializable{
 	}
 
 
-	public String getEmil() {
+	public String getEmail() {
 		return email;
 	}
 
 
-	public void setEmil(String emil) {
+	public void setEmail(String email) {
 		this.email = email;
 	}
 
